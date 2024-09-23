11141de08795750f84f09d365e97a0b81e655254
shahinur00789 <shahin00894@gmail.com>
2024-09-23 03:12:09 -0700

    history section done

--- js/style.js
+++ js/style.js
@@ -9,6 +9,27 @@ document.getElementById('donate-noakhali-btn')
 
     document.getElementById('noakhali-BDT').innerText=noakhaliNewBDT;
 
+    const balance = getTextValueById('balance');
+    const balanceRemaining = balance - donateAmountNoakhali;
+    document.getElementById('balance').innerText=balanceRemaining;
+
+    // add tanjection histry
+    const historyItem = document.createElement("div");
+historyItem.className="bg-white p-3 rounded-xl border-2 border-gray-200 space-y-4";
+
+
+
+historyItem.innerHTML=`
+<p class="font-extrabold">${donateAmountNoakhali} taka is Donate for Flood at Noakhali, Bangladesh </p>
+<p class="text-xs text-gray-500">Date:${day} ${month} ${date} ${year} ${time} ${timezone}(Bangladesh Standard Time)</p>
+`;
+
+
+const historyContainer = document.getElementById('history-list');
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+
+
 
 
 })
@@ -24,8 +45,25 @@ document.getElementById('donate-feni-btn')
 
     document.getElementById('feni-BDT').innerText=feniNewBDT;
 
+    const balance = getTextValueById('balance');
+    const balanceRemaining = balance - donateAmountFeni;
+    document.getElementById('balance').innerText=balanceRemaining;
 
-
+// history section
+    const historyItem = document.createElement("div");
+    historyItem.className="bg-white p-3 rounded-xl border-2 border-gray-200";
+    
+    
+    
+    historyItem.innerHTML=`
+    <p class="font-extrabold">${donateAmountFeni} taka is Donate for Flood Relief in Feni,Bangladesh</p>
+    <p class="text-xs text-gray-500">Date:${day} ${month} ${date} ${year} ${time} ${timezone}(Bangladesh Standard Time)</p>
+    `;
+    
+    
+    const historyContainer = document.getElementById('history-list');
+    
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 
 })
 
@@ -42,8 +80,55 @@ document.getElementById('donate-injured-btn')
 
     document.getElementById('injured-BDT').innerText=injuredNewBDT;
 
+    const balance = getTextValueById('balance');
+    const balanceRemaining = balance - donateAmountInjured;
+    document.getElementById('balance').innerText=balanceRemaining;
+
+
+    // history section
+
+    const historyItem = document.createElement("div");
+historyItem.className="bg-white p-3 rounded-xl border-2 border-gray-200";
+
+
+
+historyItem.innerHTML=`
+<p class="font-extrabold">${donateAmountInjured} taka is Donate for Aid for Injured in the Quota Movement, Bangladesh</p>
+<p class="text-xs text-gray-500">Date:${day} ${month} ${date} ${year} ${time} ${timezone}(Bangladesh Standard Time)</p>
+`;
+
+
+const historyContainer = document.getElementById('history-list');
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 
 
 
 })
 
+
+const donationTab = document.getElementById('donation-btn')
+const historiTab = document.getElementById('history-btn')
+historiTab.addEventListener('click',function(){
+    historiTab.classList.add(
+        "bg-primary-c",
+        "font-bold"
+    );
+    historiTab.classList.remove(
+        "btn-text"
+    )
+    donationTab.classList.remove(
+       "bg-primary-c",
+    "font-bold",
+    
+)
+donationTab.classList.add(
+    "btn-text"
+)
+document.getElementById('donate-card').classList.add("hidden");
+document.getElementById('history-list').classList.remove("hidden");
+
+
+ 
+
+})
--- js/utilities.js
+++ js/utilities.js
@@ -10,3 +10,18 @@ function getTextValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+
+
+
+
+
+let currentDate = new Date();
+
+
+let day = currentDate.toLocaleString('en-US', { weekday: 'short' });
+let date = currentDate.getDate(); 
+let month = currentDate.toLocaleString('en-US', { month: 'short' });  
+let year = currentDate.getFullYear(); 
+let time = currentDate.toLocaleTimeString('en-US', { hour12: false });  
+let timezone = "GMT +" + (currentDate.getTimezoneOffset() / -70); 
